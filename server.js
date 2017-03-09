76739959faebdcddf4885c9ec4acd0b828668889
Canaan Seaton <cmseaton42@gmail.com>
2017-03-09 00:31:57 -0600

    Working App, Pre-Deployment

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 var express = require('express')
-var app = express()
+var moment = require('moment');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+var app = express();
+var natLang = /(January|February|March|April|May|June|July|August|September|October|November|December)\s(\d\d?).+?(\d\d\d\d)/;
+var unixTime = /\d\d\d\d\d\d\d\d\d\d/;
+
+
+var port = process.env.PORT || 8080;
+
+
+app.get('/$', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/:date', function (req, res) {
+  var obj = {};
+  var unix = null;
+  var natural = null;
+  var param = req.params.date;
+
+  if (natLang.test(param)) {
+    natural = param;
+    unix = moment.utc(param, "MMMM D, YYYY").format("X");
+  }
+
+  if (unixTime.test(param)) {
+    var temp = moment.utc(param, "X");
+
+    natural = temp.format("MMMM D, YYYY");
+    unix = temp.format("X");
+  }
+
+  res.json({
+    "unix": unix,
+    "natural": natural
+  });
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!')
 })
